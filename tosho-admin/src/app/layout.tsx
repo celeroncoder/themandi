9195ccdba48bd476a382021ad03d81bc800ed98a
Khushal Bhardwaj <khushalbhardwaj0111@gmail.com>
2024-10-29 18:30:06 +0000

    custom scrollbar

--- tosho-admin/src/app/layout.tsx
+++ tosho-admin/src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en" className={`${GeistSans.variable}`}>
-        <body>
+        <body className="overflow-auto scroll-smooth">
           <TRPCReactProvider>{children}</TRPCReactProvider>
         </body>
       </html>
